ebd4c4999383c01842c32cb7d4a9111c5560724b
Tai <tai959328@gmail.com>
2024-07-21 23:57:05 -0400

    test commit

--- p1.py
+++ p1.py
@@ -21,3 +21,5 @@ bias = 3 # every unique neuron has a unique bias
 # output of a neuron is to add up call the inputs times the weights plus the bias
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 print(output)
+
+# test update line for github

2c17b0c4b2e15f441073cd509cabb0b2199ae264
Tai <tai959328@gmail.com>
2024-08-10 21:36:10 -0400

    testing p3

--- p2.py
+++ p2.py
@@ -37,3 +37,6 @@ output = [	inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2]
 			inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
 			inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
 print(output)
+
+#### we can't really change the inputs but we can change the weights and bias. The struggle with deep learning is
+#### figuring out how we tune the weights and biases to achieve accurate predictions
